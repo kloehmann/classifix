a65c4b5d20a4f75f19d2177fbce9452a91356fbf
Osama Al-Wardi <34874686+OsamaAl-Wardi@users.noreply.github.com>
2018-07-12 12:12:43 +0200

    Comments
    Just added some comments in the system_controller file

--- Code/app/controllers/system_controller.rb
+++ Code/app/controllers/system_controller.rb
@@ -1,16 +1,20 @@
 class SystemController < ApplicationController
+    #list all systems
     def index
         @sys = System.all
     end
     
+    #show a system
     def show
         @sys = System.find(params[:id])
     end
-
+    
+    #generate a new system object
     def new
         @sys = System.new
     end
-
+    
+    #create a new system and redirect
     def create
         @sys = System.new(system_params)
        
@@ -20,7 +24,8 @@ class SystemController < ApplicationController
           render 'new'
         end
     end
-
+    
+    #this is a definition for the allowed parameters entered to the program
     private
     def system_params
         params.require(:system).permit(:name)
