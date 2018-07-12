79eeb6e252f96cce91c4506788d1bf666e289abd
Osama Al-Wardi <34874686+OsamaAl-Wardi@users.noreply.github.com>
2018-07-12 12:03:16 +0200

    Add a System
    Created a new, create and show actions with views.

--- Code/app/controllers/system_controller.rb
+++ Code/app/controllers/system_controller.rb
@@ -2,4 +2,27 @@ class SystemController < ApplicationController
     def index
         @sys = System.all
     end
+    
+    def show
+        @sys = System.find(params[:id])
+    end
+
+    def new
+        @sys = System.new
+    end
+
+    def create
+        @sys = System.new(system_params)
+       
+        if @sys.save
+          redirect_to @sys
+        else
+          render 'new'
+        end
+    end
+
+    private
+    def system_params
+        params.require(:system).permit(:name)
+    end
 end
