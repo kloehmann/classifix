020f9a14e45d086a54078ee101bb51e895360fc4
Osama Al-Wardi <34874686+OsamaAl-Wardi@users.noreply.github.com>
2018-08-23 13:27:25 +0200

    Technical Characteristics is now done

--- Code/config/routes.rb
+++ Code/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :system
   resources :classification
+  resources :technical_characteristics
   root to: "system#index"
   root to: "classification#index"
+  root to: "technical_characteristics#index"
 end
