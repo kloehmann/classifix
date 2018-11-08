9a35e7ea97c97f95c3e89831bd0feaf89c5924c4
Kevin Löhmann <k.loehmann@neusta.de>
2018-11-08 21:18:37 +0100

    closes #16 - added prefix to classification items

--- src/app/controllers/classification_controller.rb
+++ src/app/controllers/classification_controller.rb
@@ -5,10 +5,9 @@ class ClassificationController < ApplicationController
 
         @system_id = params[:id]
         @classification = Classification.where(system: @system_id).pluck(:item_id)
-        
 
     end
-        
+
     def update
         @system_id = params[:id]
         @classification = Classification.where(system: @system_id)
@@ -17,13 +16,11 @@ class ClassificationController < ApplicationController
         end
 
         Item.all.each do |i|
-            if params.has_key?(i.key)
-                # FIXME There is an error here. The params contain the key "controller", hence controlller will always be selected.
+            if params.has_key?("ci_"+i.key)
                 c = Classification.new(system: System.find(@system_id), item: i)
                 c.save
             end
         end
         redirect_to action: "show", id: @system_id
- 
     end
 end
